d92a16037b2ed1fc53ae2dd8fd2f621487a72e04
sassybubble <srtaylor1984@gmail.com>
2018-01-08 09:00:52 +0000

    authorisation added and local token save

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
-angular.module('TodoApp', ['ngRoute', 'RouteControllers']);
+angular.module('TodoApp', ['ngRoute', 'RouteControllers', 'UserService', 'angular-storage', 'todoservice']);
  
 angular.module('TodoApp').config(function($locationProvider, $routeProvider) {
-    $locationProvider.html5Mode(true);  // Enable href routing without hashes
+    $locationProvider.html5Mode(true); 
  
     $routeProvider.when('/', {
         templateUrl: 'templates/home.html',
@@ -10,5 +10,9 @@ angular.module('TodoApp').config(function($locationProvider, $routeProvider) {
     .when('/accounts/register', {
         templateUrl: 'templates/register.html',
         controller: 'RegisterController'
+    })
+     .when('/todo', {
+        templateUrl: 'templates/todo.html',
+        controller: 'TodoController'
     });
 });
